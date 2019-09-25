10da5094bfc3ddd3bf452f99442d20f286494dcb
MikelFuentes <MikelFuentes@opendeusto.es>
2019-09-25 22:39:44 +0200

    2º pus

--- Coche/src/prin/CocheJuego.java
+++ Coche/src/prin/CocheJuego.java
@@ -1,13 +1,29 @@
 package prin;
 
+import java.awt.event.ActionEvent;
+
 public class CocheJuego extends Coche{
 	
 	private JLabelCoche dibujoCoche;
 	
 	public CocheJuego(){
-		dibujoCoche 
+		
 	}
-	
+	 public JLabelCoche getGrafico() {
+	        return this.dibujoCoche;
+	}
+	@Override
+	public void setPosX(double posX) {
+		dibujoCoche.setLocation((int)posX, (int)posY);
+		super.setPosX(posX);
+	}
+	@Override
+	public void setPosY(double posY) {
+		// TODO Auto-generated method stub
+		dibujoCoche.setLocation((int)posX, (int)posY);
+		super.setPosY(posY);
+	}
+
 	
 
 }
